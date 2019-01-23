48e19d55db5ea90948e373726523eb3f4ad12bc1
hos <hos@hos.ac>
2019-01-23 19:00:14 +0900

    Add template

---
+++ other/template.d
@@ -0,0 +1,29 @@
+import std.conv, std.stdio, std.string;
+import std.algorithm, std.array, std.bigint, std.container, std.math, std.numeric, std.range, std.regex, std.typecons;
+import core.bitop;
+
+class EOFException : Throwable { this() { super("EOF"); } }
+string[] tokens;
+string readToken() { for (; tokens.empty; ) { if (stdin.eof) { throw new EOFException; } tokens = readln.split; } auto token = tokens.front; tokens.popFront; return token; }
+int readInt() { return readToken.to!int; }
+long readLong() { return readToken.to!long; }
+real readReal() { return readToken.to!real; }
+
+bool chmin(T)(ref T t, in T f) { if (t > f) { t = f; return true; } else { return false; } }
+bool chmax(T)(ref T t, in T f) { if (t < f) { t = f; return true; } else { return false; } }
+
+int binarySearch(T)(in T[] as, in bool delegate(T) test) { int low = -1, upp = cast(int)(as.length); for (; low + 1 < upp; ) { int mid = (low + upp) >> 1; (test(as[mid]) ? low : upp) = mid; } return upp; }
+int lowerBound(T)(in T[] as, in T val) { return as.binarySearch((T a) => (a < val)); }
+int upperBound(T)(in T[] as, in T val) { return as.binarySearch((T a) => (a <= val)); }
+
+
+
+
+void main() {
+  try {
+    for (; ; ) {
+      
+    }
+  } catch (EOFException e) {
+  }
+}
