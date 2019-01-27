5a80ed0e8eec33969cfb0c0912656da8f0507464
hos <hos@hos.ac>
2019-01-27 13:37:13 +0900

    Update binarySearch (using template)

--- other/template.d
+++ other/template.d
@@ -1,4 +1,4 @@
-import std.conv, std.stdio, std.string;
+import std.conv, std.functional, std.stdio, std.string;
 import std.algorithm, std.array, std.bigint, std.container, std.math, std.numeric, std.range, std.regex, std.typecons;
 import core.bitop;
 
@@ -12,9 +12,9 @@ real readReal() { return readToken.to!real; }
 bool chmin(T)(ref T t, in T f) { if (t > f) { t = f; return true; } else { return false; } }
 bool chmax(T)(ref T t, in T f) { if (t < f) { t = f; return true; } else { return false; } }
 
-int binarySearch(T)(in T[] as, in bool delegate(T) test) { int low = -1, upp = cast(int)(as.length); for (; low + 1 < upp; ) { int mid = (low + upp) >> 1; (test(as[mid]) ? low : upp) = mid; } return upp; }
-int lowerBound(T)(in T[] as, in T val) { return as.binarySearch((T a) => (a < val)); }
-int upperBound(T)(in T[] as, in T val) { return as.binarySearch((T a) => (a <= val)); }
+int binarySearch(alias pred, T)(in T[] as) { int lo = -1, hi = cast(int)(as.length); for (; lo + 1 < hi; ) { const mid = (lo + hi) >> 1; (unaryFun!pred(as[mid]) ? hi : lo) = mid; } return hi; }
+int lowerBound(T)(in T[] as, T val) { return as.binarySearch!(a => (a >= val)); }
+int upperBound(T)(in T[] as, T val) { return as.binarySearch!(a => (a > val)); }
 
 
 
