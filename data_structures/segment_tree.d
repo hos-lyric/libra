af5c189f120621ce334cd8f9ec831520a97b1159
hos <hos@hos.ac>
2019-09-03 19:33:37 +0900

    Update segment tree binary search

--- data_structures/segment_tree.d
+++ data_structures/segment_tree.d
@@ -34,7 +34,7 @@ class SegmentTree(T, alias op, T ide) {
     update(a, opFun(ts[a + n], val));
   }
 
-  // [a, b) (0 <= a <= b <= n)
+  // prod of [a, b) (0 <= a <= b <= n)
   T rangeProd(int a, int b) const {
     T prodL = ide, prodR = ide;
     for (a += n, b += n; a < b; a >>= 1, b >>= 1) {
@@ -44,23 +44,52 @@ class SegmentTree(T, alias op, T ide) {
     return opFun(prodL, prodR);
   }
 
-  // min pos s.t. pred(sum of [0, pos))
-  //   assume pred(sum of [0, pos)) is non-decreasing
-  int binarySearch(alias pred)() const {
-    import std.functional : unaryFun;
-    alias predFun = unaryFun!pred;
-    if (predFun(ide)) return 0;
-    if (!predFun(ts[1])) return n + 1;
+  // min b s.t. pred(prod of [a, b)) (or n + 1 if no such b)
+  //   0 <= a <= n
+  //   assume pred(prod of [a, b)) is non-decreasing in b
+  int binarySearchR(int a, bool delegate(T) pred) const {
+    if (pred(ide)) return a;
+    if (a == n) return n + 1;
     T prod = ide;
-    int a = 1;
-    for (; a < n; ) {
-      a <<= 1;
-      if (!predFun(opFun(prod, ts[a]))) {
-        prod = opFun(prod, ts[a]);
-        a |= 1;
+    for (a += n; ; a >>= 1) {
+      if (a & 1) {
+        if (pred(opFun(prod, ts[a]))) {
+          for (; a < n; ) {
+            a <<= 1;
+            if (!pred(opFun(prod, ts[a]))) {
+              prod = opFun(prod, ts[a++]);
+            }
+          }
+          return a - n + 1;
+        }
+        prod = opFun(prod, ts[a++]);
+        if (!(a & a - 1)) return n + 1;
       }
     }
-    return a - n + 1;
+  }
+
+  // max a s.t. pred(prod of [a, b)) (or -1 if no such a)
+  //   0 <= b <= n
+  //   assume pred(prod of [a, b)) is non-increasing in a
+  int binarySearchL(int b, bool delegate(T) pred) const {
+    if (pred(ide)) return b;
+    if (b == 0) return -1;
+    T prod = ide;
+    for (b += n; ; b >>= 1) {
+      if ((b & 1) || b == 2) {
+        if (pred(opFun(prod, ts[b - 1]))) {
+          for (; b <= n; ) {
+            b <<= 1;
+            if (!pred(opFun(prod, ts[b - 1]))) {
+              prod = opFun(prod, ts[--b]);
+            }
+          }
+          return b - n - 1;
+        }
+        prod = opFun(prod, ts[--b]);
+        if (!(b & b - 1)) return -1;
+      }
+    }
   }
 }
 
@@ -77,10 +106,22 @@ unittest {
   seg.update(21, "V");
   assert(seg.get(17) == "^r");
   assert(seg.rangeProd(15, 21) == "Pq^rst$u");
-  assert(seg.binarySearch!"a == a" == 0);
-  assert(seg.binarySearch!"a.indexOf('q') != -1" == 17);
-  assert(seg.binarySearch!"a.indexOf('^') != -1" == 18);
-  assert(seg.binarySearch!"a.indexOf('?') != -1" == seg.n + 1);
+}
+
+// binarySearch
+unittest {
+  import std.algorithm : max, min;
+  enum n = 1 << 4;
+  auto seg = new SegmentTree!(int, "a + b", 0)(n, new int[n]);
+  foreach (i; 0 .. n) {
+    seg.mulR(i, 1);
+  }
+  foreach (i; 0 .. n + 1) {
+    foreach (d; 0 .. n + 2) {
+      assert(seg.binarySearchR(i, s => (s >= d)) == min(i + d, n + 1));
+      assert(seg.binarySearchL(i, s => (s >= d)) == max(i - d, -1));
+    }
+  }
 }
 
 void main() {
