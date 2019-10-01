ed48713b16157bcd70a3a8e4a7afa09ff7669732
hos <hos@hos.ac>
2019-10-01 19:47:50 +0900

    Add tem

---
+++ other/tem.cpp
@@ -0,0 +1,42 @@
+// #pragma GCC optimize ("Ofast")
+// #pragma GCC optimize ("unroll-loops")
+// #pragma GCC target ("avx")
+
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using Int = long long;
+
+template <class T1, class T2> ostream &operator<<(ostream &os, const pair<T1, T2> &a) { return os << "(" << a.first << ", " << a.second << ")"; };
+template <class T> void pv(T a, T b) { for (T i = a; i != b; ++i) cerr << *i << " "; cerr << endl; }
+template <class T> void chmin(T &t, const T &f) { if (t > f) t = f; }
+template <class T> void chmax(T &t, const T &f) { if (t < f) t = f; }
+
+
+
+int main() {
+  
+  
+  return 0;
+}
