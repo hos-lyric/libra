d6409295d1d361c0bee44a19fd94a43cf94d947d
hos <hos@hos.ac>
2020-02-21 05:29:26 +0900

    Separate mod_inv into module and test

--- number/mod_inv.d
+++ number/mod_inv.d
@@ -1,3 +1,5 @@
+module mod_inv;
+
 // a^-1 (mod 2^64)
 long modInv(long a)
 in {
@@ -35,30 +37,4 @@ do {
     }
     y -= t * x;
   }
-}
-
-// modInv
-unittest {
-  for (long a = -99; a <= 99; a += 2) {
-    assert(a * modInv(a) == 1);
-  }
-  assert(123456789123456789L * modInv(123456789123456789L) == 1);
-  foreach (m; 1 .. 10) {
-    foreach (a; -9 .. 100) {
-      bool coprime = true;
-      foreach (g; 2 .. 100) {
-        if (m % g == 0 && a % g == 0) {
-          coprime = false;
-        }
-      }
-      if (coprime) {
-        const b = modInv(a, m);
-        assert(0 <= b && b < m);
-        assert((a * b - 1) % m == 0);
-      }
-    }
-  }
-}
-
-void main() {
 }
