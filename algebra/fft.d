4eccceacca394411bf1d89ae3d7b29d7428bfe63
hos <hos@hos.ac>
2020-03-04 14:21:56 +0900

    Fix naming

--- algebra/fft.d
+++ algebra/fft.d
@@ -1,4 +1,4 @@
-// dmd -O -unittest fft ../number/modint.d ../number/mod_inv.d
+// dmd -O -unittest fft ../number/mod_inv
 import modint, mod_inv;
 
 // M: prime, G: primitive root
@@ -47,7 +47,7 @@ class Fft(int M_, int G, int K) {
       for (int l = n; (l >>= 1) && !((j ^= l) & l); ) {}
     }
   }
-  T[] convolution(T)(inout(T)[] as, inout(T)[] bs) const if (isIntegral!T) {
+  T[] convolute(T)(inout(T)[] as, inout(T)[] bs) const if (isIntegral!T) {
     const na = cast(int)(as.length), nb = cast(int)(bs.length);
     int n, invN = 1;
     for (n = 1; n < na + nb - 1; n <<= 1) {
@@ -67,7 +67,7 @@ class Fft(int M_, int G, int K) {
     foreach (i; 0 .. na + nb - 1) cs[i] = cast(T)(xs[i]);
     return cs;
   }
-  ModInt!M[] convolution(inout(ModInt!M)[] as, inout(ModInt!M)[] bs) const {
+  ModInt!M[] convolute(inout(ModInt!M)[] as, inout(ModInt!M)[] bs) const {
     const na = cast(int)(as.length), nb = cast(int)(bs.length);
     int n, invN = 1;
     for (n = 1; n < na + nb - 1; n <<= 1) {
@@ -87,7 +87,7 @@ class Fft(int M_, int G, int K) {
     foreach (i; 0 .. na + nb - 1) cs[i].x = xs[i];
     return cs;
   }
-  int[] convolution(int M1)(inout(ModInt!M1)[] as, inout(ModInt!M1)[] bs) const
+  int[] convolute(int M1)(inout(ModInt!M1)[] as, inout(ModInt!M1)[] bs) const
       if (M != M1) {
     const na = cast(int)(as.length), nb = cast(int)(bs.length);
     int n, invN = 1;
@@ -108,68 +108,76 @@ class Fft(int M_, int G, int K) {
   }
 }
 
+alias Fft0 = Fft!(998244353, 3, 20);
+
+
+// Fft3_0.M Fft3_1.M Fft3_2.M > 1.15 * 10^27, > 2^89.9
+//*
 enum FFT_K = 20;
-alias Fft0 = Fft!(1045430273, 3, FFT_K);  // 2^20 997 + 1
-alias Fft1 = Fft!(1051721729, 6, FFT_K);  // 2^20 1003 + 1
-alias Fft2 = Fft!(1053818881, 7, FFT_K);  // 2^20 1005 + 1
-// Fft0.M Fft1.M Fft2.M > 5.95 * 10^25 > 2^85.6
-// enum FFT_K = 24;
-// alias Fft0 = Fft!(167772161, 3, FFT_K);  // 2^25 5 + 1
-// alias Fft1 = Fft!(469762049, 3, FFT_K);  // 2^26 7 + 1
-// alias Fft2 = Fft!(754974721, 11, FFT_K);  // 2^24 45 + 1
-enum long FFT_INV01 = modInv(Fft0.M, Fft1.M);
-enum long FFT_INV012 = modInv(cast(long)(Fft0.M) * Fft1.M, Fft2.M);
-Fft0 FFT0;
-Fft1 FFT1;
-Fft2 FFT2;
-void fftInit() {
-  FFT0 = new Fft0;
-  FFT1 = new Fft1;
-  FFT2 = new Fft2;
+alias Fft3_0 = Fft!(1045430273, 3, FFT_K);  // 2^20 997 + 1
+alias Fft3_1 = Fft!(1051721729, 6, FFT_K);  // 2^20 1003 + 1
+alias Fft3_2 = Fft!(1053818881, 7, FFT_K);  // 2^20 1005 + 1
+//*/
+// Fft3_0.M Fft3_1.M Fft3_2.M > 5.95 * 10^25, > 2^85.6
+/*
+enum FFT_K = 24;
+alias Fft3_0 = Fft!(167772161, 3, FFT_K);  // 2^25 5 + 1
+alias Fft3_1 = Fft!(469762049, 3, FFT_K);  // 2^26 7 + 1
+alias Fft3_2 = Fft!(754974721, 11, FFT_K);  // 2^24 45 + 1
+//*/
+enum long FFT_INV01 = modInv(Fft3_0.M, Fft3_1.M);
+enum long FFT_INV012 = modInv(cast(long)(Fft3_0.M) * Fft3_1.M, Fft3_2.M);
+Fft3_0 FFT3_0;
+Fft3_1 FFT3_1;
+Fft3_2 FFT3_2;
+void initFft3() {
+  FFT3_0 = new Fft3_0;
+  FFT3_1 = new Fft3_1;
+  FFT3_2 = new Fft3_2;
 }
-long[] convolution(inout(long)[] as, inout(long)[] bs) {
-  const cs0 = FFT0.convolution(as, bs);
-  const cs1 = FFT1.convolution(as, bs);
-  const cs2 = FFT2.convolution(as, bs);
+long[] convolute(inout(long)[] as, inout(long)[] bs) {
+  const cs0 = FFT3_0.convolute(as, bs);
+  const cs1 = FFT3_1.convolute(as, bs);
+  const cs2 = FFT3_2.convolute(as, bs);
   auto cs = new long[cs0.length];
   foreach (i; 0 .. cs0.length) {
-    long d0 = cs0[i] % Fft0.M;
-    long d1 = (FFT_INV01 * (cs1[i] - d0)) % Fft1.M;
-    if (d1 < 0) d1 += Fft1.M;
-    long d2 = (FFT_INV012 * ((cs2[i] - d0 - Fft0.M * d1) % Fft2.M)) % Fft2.M;
-    if (d2 < 0) d2 += Fft2.M;
-    cs[i] = d0 + Fft0.M * d1 + (cast(long)(Fft0.M) * Fft1.M) * d2;
+    long d0 = cs0[i] % Fft3_0.M;
+    long d1 = (FFT_INV01 * (cs1[i] - d0)) % Fft3_1.M;
+    if (d1 < 0) d1 += Fft3_1.M;
+    long d2 = (FFT_INV012 * ((cs2[i] - d0 - Fft3_0.M * d1) % Fft3_2.M)) % Fft3_2.M;
+    if (d2 < 0) d2 += Fft3_2.M;
+    cs[i] = d0 + Fft3_0.M * d1 + (cast(long)(Fft3_0.M) * Fft3_1.M) * d2;
   }
   return cs;
 }
-long[] convolution(inout(long)[] as, inout(long)[] bs, long m) {
-  const cs0 = FFT0.convolution(as, bs);
-  const cs1 = FFT1.convolution(as, bs);
-  const cs2 = FFT2.convolution(as, bs);
+long[] convolute(inout(long)[] as, inout(long)[] bs, long m) {
+  const cs0 = FFT3_0.convolute(as, bs);
+  const cs1 = FFT3_1.convolute(as, bs);
+  const cs2 = FFT3_2.convolute(as, bs);
   auto cs = new long[cs0.length];
   foreach (i; 0 .. cs0.length) {
-    long d0 = cs0[i] % Fft0.M;
-    long d1 = (FFT_INV01 * (cs1[i] - d0)) % Fft1.M;
-    if (d1 < 0) d1 += Fft1.M;
-    long d2 = (FFT_INV012 * ((cs2[i] - d0 - Fft0.M * d1) % Fft2.M)) % Fft2.M;
-    if (d2 < 0) d2 += Fft2.M;
-    cs[i] = (d0 + Fft0.M * d1 + ((cast(long)(Fft0.M) * Fft1.M) % m) * d2) % m;
+    long d0 = cs0[i] % Fft3_0.M;
+    long d1 = (FFT_INV01 * (cs1[i] - d0)) % Fft3_1.M;
+    if (d1 < 0) d1 += Fft3_1.M;
+    long d2 = (FFT_INV012 * ((cs2[i] - d0 - Fft3_0.M * d1) % Fft3_2.M)) % Fft3_2.M;
+    if (d2 < 0) d2 += Fft3_2.M;
+    cs[i] = (d0 + Fft3_0.M * d1 + ((cast(long)(Fft3_0.M) * Fft3_1.M) % m) * d2) % m;
   }
   return cs;
 }
-ModInt!M[] convolution(int M)(inout(ModInt!M)[] as, inout(ModInt!M)[] bs) {
+ModInt!M[] convolute(int M)(inout(ModInt!M)[] as, inout(ModInt!M)[] bs) {
   auto asInt = new int[as.length], bsInt = new int[bs.length];
-  const cs0 = FFT0.convolution(as, bs);
-  const cs1 = FFT1.convolution(as, bs);
-  const cs2 = FFT2.convolution(as, bs);
+  const cs0 = FFT3_0.convolute(as, bs);
+  const cs1 = FFT3_1.convolute(as, bs);
+  const cs2 = FFT3_2.convolute(as, bs);
   auto cs = new ModInt!M[cs0.length];
   foreach (i; 0 .. cs0.length) {
-    long d0 = cs0[i] % Fft0.M;
-    long d1 = (FFT_INV01 * (cs1[i] - d0)) % Fft1.M;
-    if (d1 < 0) d1 += Fft1.M;
-    long d2 = (FFT_INV012 * ((cs2[i] - d0 - Fft0.M * d1) % Fft2.M)) % Fft2.M;
-    if (d2 < 0) d2 += Fft2.M;
-    cs[i] = (d0 + Fft0.M * d1 + ((cast(long)(Fft0.M) * Fft1.M) % M) * d2) % M;
+    long d0 = cs0[i] % Fft3_0.M;
+    long d1 = (FFT_INV01 * (cs1[i] - d0)) % Fft3_1.M;
+    if (d1 < 0) d1 += Fft3_1.M;
+    long d2 = (FFT_INV012 * ((cs2[i] - d0 - Fft3_0.M * d1) % Fft3_2.M)) % Fft3_2.M;
+    if (d2 < 0) d2 += Fft3_2.M;
+    cs[i] = (d0 + Fft3_0.M * d1 + ((cast(long)(Fft3_0.M) * Fft3_1.M) % M) * d2) % M;
   }
   return cs;
 }
@@ -180,27 +188,27 @@ unittest {
   int[] as = [31, 41, 59, 26, 53];
   const(int)[] bs = [58, 9, 79, 32, 38, 46];
   const cs = [52, 38, 32, 62, 80, 31, 29, 63, 9, 13];
-  assert(fft.convolution(as, bs) == cs);
+  assert(fft.convolute(as, bs) == cs);
 }
 
 unittest {
   enum MO = 998244353;
   alias Mint = ModInt!MO;
-  auto fft = new Fft!(MO, 3, 20);
+  auto fft = new Fft0;
   auto as = [Mint(1), Mint(1)];
   auto bs = [Mint(-1), Mint(-1), Mint(-1)];
-  assert(fft.convolution(as, bs) == [Mint(-1), Mint(-2), Mint(-2), Mint(-1)]);
+  assert(fft.convolute(as, bs) == [Mint(-1), Mint(-2), Mint(-2), Mint(-1)]);
 }
 
 unittest {
-  fftInit;
+  initFft3;
   enum as = [1 * 10L^^10, 2 * 10L^^10, 3 * 10L^^10];
   enum bs = [4 * 10L^^7, 5 * 10L^^7, 6 * 10L^^7, 7 * 10L^^7];
   enum cs = [4 * 10L^^17, 13 * 10L^^17, 28 * 10L^^17, 34 * 10L^^17,
              32 * 10L^^17, 21 * 10L^^17];
-  assert(convolution(as, bs) == cs);
+  assert(convolute(as, bs) == cs);
   enum m = 1_234_567_890_123L;
-  const res = convolution(as, bs, m);
+  const res = convolute(as, bs, m);
   assert(res.length == 6);
   foreach (i; 0 .. 6) {
     assert(res[i] == cs[i] % m);
@@ -208,11 +216,12 @@ unittest {
 }
 
 unittest {
+  initFft3;
   enum MO = 10^^9 + 7;
   alias Mint = ModInt!MO;
   auto as = [Mint(1), Mint(1)];
   auto bs = [Mint(-1), Mint(-1), Mint(-1)];
-  assert(convolution(as, bs) == [Mint(-1), Mint(-2), Mint(-2), Mint(-1)]);
+  assert(convolute(as, bs) == [Mint(-1), Mint(-2), Mint(-2), Mint(-1)]);
 }
 
 void main() {
