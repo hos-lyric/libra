541e933fda1ddbbf4eab313a266333778b60589a
hos <hos@hos.ac>
2020-11-07 06:53:42 +0900

    Add division free det

---
+++ algebra/matrix.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+
+#include "modint.h"
+
+using std::vector;
+
+// det(t I - A), division free, O(n^4)
+template <class T> vector<T> charPoly(const vector<vector<T>> &a) {
+  const int n = a.size();
+  vector<T> ps(n + 1, 0);
+  ps[n] = 1;
+  for (int h = n - 1; h >= 0; --h) {
+    // closed walks at h with repetition allowed from 0, ..., h - 1
+    vector<vector<T>> sub(n, vector<T>(h + 1, 0));
+    for (int i = n; i >= 1; --i) {
+      sub[i - 1][h] += ps[i];
+    }
+    for (int i = n - 1; i >= 1; --i) for (int u = 0; u <= h; ++u) {
+      for (int v = 0; v < h; ++v) {
+        sub[i - 1][v] -= sub[i][u] * a[u][v];
+      }
+    }
+    for (int i = n - 1; i >= 0; --i) for (int u = 0; u <= h; ++u) {
+      ps[i] += sub[i][u] * a[u][h];
+    }
+  }
+  for (int i = n - 1; i >= 0; i -= 2) {
+    ps[i] *= -1;
+  }
+  return ps;
+}
+
+
+constexpr int MO = 998244353;
+using Mint = ModInt<MO>;
+
+void unittest() {
+  vector<vector<Mint>> a{{3, 1, 4}, {1, 5, 9}, {2, 6, 5}};
+  const auto ps = charPoly(a);
+  assert(ps[0].x == 90);
+  assert(ps[1].x == MO - 8);
+  assert(ps[2].x == MO - 13);
+  assert(ps[3].x == 1);
+}
+
+int main() {
+  unittest();
+  return 0;
+}
---
+++ algebra/modint.h
@@ -0,0 +1,48 @@
+#include <cassert>
+#include <iostream>
+
+template<int M_> struct ModInt {
+  static constexpr int M = M_;
+  int x;
+  constexpr ModInt() : x(0) {}
+  constexpr ModInt(long long x_) : x(x_ % M) { if (x < 0) x += M; }
+  ModInt &operator+=(const ModInt &a) { x += a.x; if (x >= M) x -= M; return *this; }
+  ModInt &operator-=(const ModInt &a) { x -= a.x; if (x < 0) x += M; return *this; }
+  ModInt &operator*=(const ModInt &a) { x = static_cast<int>((static_cast<long long>(x) * a.x) % M); return *this; }
+  ModInt &operator/=(const ModInt &a) { return (*this *= a.inv()); }
+  ModInt operator+(const ModInt &a) const { return (ModInt(*this) += a); }
+  ModInt operator-(const ModInt &a) const { return (ModInt(*this) -= a); }
+  ModInt operator*(const ModInt &a) const { return (ModInt(*this) *= a); }
+  ModInt operator/(const ModInt &a) const { return (ModInt(*this) /= a); }
+  ModInt operator-() const { return ModInt(-x); }
+  ModInt pow(long long e) const {
+    if (e < 0) return inv().pow(-e);
+    ModInt x2 = x, xe = 1;
+    for (; e; e >>= 1) {
+      if (e & 1) xe *= x2;
+      x2 *= x2;
+    }
+    return xe;
+  }
+  ModInt inv() const {
+    int a = x, b = M, y = 1, z = 0, t;
+    for (; ; ) {
+      t = a / b; a -= t * b;
+      if (a == 0) {
+        assert(b == 1 || b == -1);
+        return ModInt(b * z);
+      }
+      y -= t * z;
+      t = b / a; b -= t * a;
+      if (b == 0) {
+        assert(a == 1 || a == -1);
+        return ModInt(a * y);
+      }
+      z -= t * y;
+    }
+  }
+  friend ModInt operator+(long long a, const ModInt &b) { return (ModInt(a) += b); }
+  friend ModInt operator-(long long a, const ModInt &b) { return (ModInt(a) -= b); }
+  friend ModInt operator*(long long a, const ModInt &b) { return (ModInt(a) *= b); }
+  friend std::ostream &operator<<(std::ostream &os, const ModInt &a) { return os << a.x; }
+};
