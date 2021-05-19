44e6c6dbbd5a4b68a800714f2676273c4aa60ef9
hos <hos@hos.ac>
2021-05-19 14:20:35 +0900

    [WIP] Update fft for any mod

--- algebra/fft.cpp
+++ algebra/fft.cpp
@@ -1,153 +1,174 @@
 #include <assert.h>
-#include <stdio.h>
-#include <algorithm>
+#include <string.h>
 #include <vector>
+
+#include "modint.h"
 
 using std::vector;
 
-// M: prime, G: primitive root
-template <int M, int G, int K> struct Fft {
-  // 1, 1/4, 1/8, 3/8, 1/16, 5/16, 3/16, 7/16, ...
-  int gs[1 << (K - 1)];
-  constexpr Fft() : gs() {
-    static_assert(2 <= K && K <= 30, "Fft: 2 <= K <= 30 must hold");
-    static_assert(!((M - 1) & ((1 << K) - 1)), "Fft: 2^K | M - 1 must hold");
-    gs[0] = 1;
-    long long g2 = G, gg = 1;
-    for (int e = (M - 1) >> K; e; e >>= 1) {
-      if (e & 1) gg = (gg * g2) % M;
-      g2 = (g2 * g2) % M;
+////////////////////////////////////////////////////////////////////////////////
+// M: prime, G: primitive root, 2^K | M - 1
+template <unsigned M_, unsigned G_, int K_> struct Fft {
+  static_assert(2U <= M_, "Fft: 2 <= M must hold.");
+  static_assert(M_ < 1U << 30, "Fft: M < 2^30 must hold.");
+  static_assert(1 <= K_, "Fft: 1 <= K must hold.");
+  static_assert(K_ < 30, "Fft: K < 30 must hold.");
+  static_assert(!((M_ - 1U) & ((1U << K_) - 1U)), "Fft: 2^K | M - 1 must hold.");
+  static constexpr unsigned M = M_;
+  static constexpr unsigned M2 = 2U * M_;
+  static constexpr ModInt<M> G = G_;
+  static constexpr int K = K_;
+  ModInt<M> FFT_ROOTS[K + 1], INV_FFT_ROOTS[K + 1], FFT_RATIOS[K - 1], INV_FFT_RATIOS[K - 1];
+  Fft() {
+    for (int k = 0; k <= K; ++k) {
+      FFT_ROOTS[k] = G.pow((M - 1U) >> k);
+      INV_FFT_ROOTS[k] = FFT_ROOTS[k].inv();
     }
-    gs[1 << (K - 2)] = gg;
-    for (int l = 1 << (K - 2); l >= 2; l >>= 1) {
-      gs[l >> 1] = (static_cast<long long>(gs[l]) * gs[l]) % M;
+    for (int k = 0; k <= K - 2; ++k) {
+      FFT_RATIOS[k] = -G.pow(3U * ((M - 1U) >> (k + 2)));
+      INV_FFT_RATIOS[k] = FFT_RATIOS[k].inv();
     }
-    assert((static_cast<long long>(gs[1]) * gs[1]) % M == M - 1);
-    for (int l = 2; l <= 1 << (K - 2); l <<= 1) {
-      for (int i = 1; i < l; ++i) {
-        gs[l + i] = (static_cast<long long>(gs[l]) * gs[i]) % M;
+    assert(FFT_ROOTS[1].x == M - 1U);
+  }
+  // as[rev(i)] <- \sum_j \zeta^(ij) as[j]
+  void fft(ModInt<M> *as, int n) const {
+    assert(!(n & (n - 1))); assert(1 <= n); assert(n <= 1 << K);
+    int m = n;
+    if (m >>= 1) {
+      for (int i = 0; i < m; ++i) {
+        const unsigned x = as[i + m].x;  // < M
+        as[i + m].x = as[i].x + M - x;  // < 2 M
+        as[i].x += x;  // < 2 M
       }
     }
-  }
-  void fft(vector<int> &xs) const {
-    const int n = xs.size();
-    assert(!(n & (n - 1)) && n <= 1 << K);
-    for (int l = n; l >>= 1; ) {
-      for (int i = 0; i < (n >> 1) / l; ++i) {
-        const long long g = gs[i];
-        for (int j = (i << 1) * l; j < (i << 1 | 1) * l; ++j) {
-          const int t = (g * xs[j + l]) % M;
-          if ((xs[j + l] = xs[j] - t) < 0) xs[j + l] += M;
-          if ((xs[j] += t) >= M) xs[j] -= M;
+    if (m >>= 1) {
+      ModInt<M> prod = 1;
+      for (int h = 0, i0 = 0; i0 < n; i0 += (m << 1)) {
+        for (int i = i0; i < i0 + m; ++i) {
+          const unsigned x = (prod * as[i + m]).x;  // < M
+          as[i + m].x = as[i].x + M - x;  // < 3 M
+          as[i].x += x;  // < 3 M
+        }
+        prod *= FFT_RATIOS[__builtin_ctz(++h)];
+      }
+    }
+    for (; m; ) {
+      if (m >>= 1) {
+        ModInt<M> prod = 1;
+        for (int h = 0, i0 = 0; i0 < n; i0 += (m << 1)) {
+          for (int i = i0; i < i0 + m; ++i) {
+            const unsigned x = (prod * as[i + m]).x;  // < M
+            as[i + m].x = as[i].x + M - x;  // < 4 M
+            as[i].x += x;  // < 4 M
+          }
+          prod *= FFT_RATIOS[__builtin_ctz(++h)];
+        }
+      }
+      if (m >>= 1) {
+        ModInt<M> prod = 1;
+        for (int h = 0, i0 = 0; i0 < n; i0 += (m << 1)) {
+          for (int i = i0; i < i0 + m; ++i) {
+            const unsigned x = (prod * as[i + m]).x;  // < M
+            as[i].x = (as[i].x >= M2) ? (as[i].x - M2) : as[i].x;  // < 2 M
+            as[i + m].x = as[i].x + M - x;  // < 3 M
+            as[i].x += x;  // < 3 M
+          }
+          prod *= FFT_RATIOS[__builtin_ctz(++h)];
         }
       }
     }
-  }
-  void invFft(vector<int> &xs) const {
-    const int n = xs.size();
-    assert(!(n & (n - 1)) && n <= 1 << K);
-    for (int l = 1; l < n; l <<= 1) {
-      std::reverse(xs.begin() + l, xs.begin() + (l << 1));
-    }
-    for (int l = 1; l < n; l <<= 1) {
-      for (int i = 0; i < (n >> 1) / l; ++i) {
-        const long long g = gs[i];
-        for (int j = (i << 1) * l; j < (i << 1 | 1) * l; ++j) {
-          int t = (g * (xs[j] - xs[j + l])) % M;
-          if (t < 0) t += M;
-          if ((xs[j] += xs[j + l]) >= M) xs[j] -= M;
-          xs[j + l] = t;
-        }
-      }
-    }
-  }
-  template<class T>
-  vector<T> convolute(const vector<T> &as, const vector<T> &bs) const {
-    const int na = as.size(), nb = bs.size();
-    int n, invN = 1;
-    for (n = 1; n < na + nb - 1; n <<= 1) {
-      invN = ((invN & 1) ? (invN + M) : invN) >> 1;
-    }
-    vector<int> xs(n, 0), ys(n, 0);
-    for (int i = 0; i < na; ++i) if ((xs[i] = as[i] % M) < 0) xs[i] += M;
-    for (int i = 0; i < nb; ++i) if ((ys[i] = bs[i] % M) < 0) ys[i] += M;
-    fft(xs);
-    fft(ys);
     for (int i = 0; i < n; ++i) {
-      xs[i] = (((static_cast<long long>(xs[i]) * ys[i]) % M) * invN) % M;
+      as[i].x = (as[i].x >= M2) ? (as[i].x - M2) : as[i].x;  // < 2 M
+      as[i].x = (as[i].x >= M) ? (as[i].x - M) : as[i].x;  // < M
     }
-    invFft(xs);
-    xs.resize(na + nb - 1);
-    return xs;
+  }
+  // as[i] <- (1/n) \sum_j \zeta^(-ij) as[rev(j)]
+  void invFft(ModInt<M> *as, int n) const {
+    assert(!(n & (n - 1))); assert(1 <= n); assert(n <= 1 << K);
+    int m = 1;
+    if (m < n >> 1) {
+      ModInt<M> prod = 1;
+      for (int h = 0, i0 = 0; i0 < n; i0 += (m << 1)) {
+        for (int i = i0; i < i0 + m; ++i) {
+          const unsigned long long y = as[i].x + M - as[i + m].x;  // < 2 M
+          as[i].x += as[i + m].x;  // < 2 M
+          as[i + m].x = (prod.x * y) % M;  // < M
+        }
+        prod *= INV_FFT_RATIOS[__builtin_ctz(++h)];
+      }
+      m <<= 1;
+    }
+    for (; m < n >> 1; m <<= 1) {
+      ModInt<M> prod = 1;
+      for (int h = 0, i0 = 0; i0 < n; i0 += (m << 1)) {
+        for (int i = i0; i < i0 + (m >> 1); ++i) {
+          const unsigned long long y = as[i].x + M2 - as[i + m].x;  // < 4 M
+          as[i].x += as[i + m].x;  // < 4 M
+          as[i].x = (as[i].x >= M2) ? (as[i].x - M2) : as[i].x;  // < 2 M
+          as[i + m].x = (prod.x * y) % M;  // < M
+        }
+        for (int i = i0 + (m >> 1); i < i0 + m; ++i) {
+          const unsigned long long y = as[i].x + M - as[i + m].x;  // < 2 M
+          as[i].x += as[i + m].x;  // < 2 M
+          as[i + m].x = (prod.x * y) % M;  // < M
+        }
+        prod *= INV_FFT_RATIOS[__builtin_ctz(++h)];
+      }
+    }
+    if (m < n) {
+      for (int i = 0; i < m; ++i) {
+        const unsigned y = as[i].x + M2 - as[i + m].x;  // < 4 M
+        as[i].x += as[i + m].x;  // < 4 M
+        as[i + m].x = y;  // < 4 M
+      }
+    }
+    const ModInt<M> invN = ModInt<M>(n).inv();
+    for (int i = 0; i < n; ++i) {
+      as[i] *= invN;
+    }
+  }
+  void fft(vector<ModInt<M>> &as) const {
+    fft(as.data(), as.size());
+  }
+  void invFft(vector<ModInt<M>> &as) const {
+    invFft(as.data(), as.size());
+  }
+  vector<ModInt<M>> convolve(vector<ModInt<M>> as, vector<ModInt<M>> bs) const {
+    if (as.empty() || bs.empty()) return {};
+    const int len = as.size() + bs.size() - 1;
+    int n = 1;
+    for (; n < len; n <<= 1) {}
+    as.resize(n); fft(as);
+    bs.resize(n); fft(bs);
+    for (int i = 0; i < n; ++i) as[i] *= bs[i];
+    invFft(as);
+    as.resize(len);
+    return as;
   }
 };
 
-const Fft<998244353, 3, 20> FFT;
+const Fft<998244353, 3, 23> FFT998244353;
+
+// TODO: convolve mod non-FFT-friendly or mod 2^64
+////////////////////////////////////////////////////////////////////////////////
 
 void unittest() {
-  constexpr Fft<97, 92, 5> FFT97;
-  const vector<int> a{31, 41, 59, 26, 53};
-  const vector<int> b{58, 9, 79, 32, 38, 46};
-  const vector<int> c{52, 38, 32, 62, 80, 31, 29, 63, 9, 13};
-  assert(FFT97.convolute(a, b) == c);
-}
-
-
-// https://judge.yosupo.jp/problem/convolution_mod
-int readInt() {
-  int c;
-  for (; ; ) {
-    c = getchar();
-    if ('0' <= c && c <= '9') break;
-    if (c == -1) throw -1;
-    if (c == '-') return -readInt();
+  {
+    assert(FFT998244353.FFT_ROOTS[23] == 15311432U);
+    assert(FFT998244353.INV_FFT_ROOTS[23] == 469870224U);
+    assert(FFT998244353.FFT_RATIOS[21] == 867605899U);
+    assert(FFT998244353.INV_FFT_RATIOS[21] == 103369235U);
   }
-  int x = c - '0';
-  for (; ; ) {
-    c = getchar();
-    if (!('0' <= c && c <= '9')) return x;
-    x = x * 10 + (c - '0');
-  }
-}
-char writeIntBuffer[10];
-void writeInt(int x) {
-  if (x < 0) {
-    putchar('-');
-    x = -x;
-  }
-  int i = 0;
-  do {
-    writeIntBuffer[i++] = '0' + (x % 10);
-    x /= 10;
-  } while (x != 0);
-  for (; i > 0; ) {
-    putchar(writeIntBuffer[--i]);
+  {
+    const Fft<97, 92, 5> FFT;
+    const vector<ModInt<FFT.M>> as{31, 41, 59, 26, 53};
+    const vector<ModInt<FFT.M>> bs{58, 9, 79, 32, 38, 46};
+    const vector<ModInt<FFT.M>> cs{52, 38, 32, 62, 80, 31, 29, 63, 9, 13};
+    assert(FFT.convolve(as, bs) == cs);
   }
 }
 
 int main() {
-  // unittest();
-
-  try {
-    for (; ; ) {
-      const int L = readInt();
-      const int M = readInt();
-      vector<int> A(L), B(M);
-      for (int i = 0; i < L; ++i) {
-        A[i] = readInt();
-      }
-      for (int i = 0; i < M; ++i) {
-        B[i] = readInt();
-      }
-
-      const vector<int> res = FFT.convolute(A, B);
-      for (int i = 0; i < L + M - 1; ++i) {
-        if (i > 0) putchar(' ');
-        writeInt(res[i]);
-      }
-      putchar('\n');
-    }
-  } catch (int) {
-  }
+  unittest();
   return 0;
 }
