45d7b705a6def0878e245b36ceace230c9dbab24
hos <hos@hos.ac>
2021-06-02 03:16:40 +0900

    Fix modint

--- algebra/modint.h
+++ algebra/modint.h
@@ -8,7 +8,7 @@
 template <unsigned M_> struct ModInt {
   static constexpr unsigned M = M_;
   unsigned x;
-  constexpr ModInt() : x(0) {}
+  constexpr ModInt() : x(0U) {}
   constexpr ModInt(unsigned x_) : x(x_ % M) {}
   constexpr ModInt(unsigned long long x_) : x(x_ % M) {}
   constexpr ModInt(int x_) : x(((x_ %= static_cast<int>(M)) < 0) ? (x_ + static_cast<int>(M)) : x_) {}
@@ -19,15 +19,15 @@ template <unsigned M_> struct ModInt {
   ModInt &operator/=(const ModInt &a) { return (*this *= a.inv()); }
   ModInt pow(long long e) const {
     if (e < 0) return inv().pow(-e);
-    ModInt a = *this, b = 1; for (; e; e >>= 1) { if (e & 1) b *= a; a *= a; } return b;
+    ModInt a = *this, b = 1U; for (; e; e >>= 1) { if (e & 1) b *= a; a *= a; } return b;
   }
   ModInt inv() const {
     unsigned a = M, b = x; int y = 0, z = 1;
     for (; b; ) { const unsigned q = a / b; const unsigned c = a - q * b; a = b; b = c; const int w = y - static_cast<int>(q) * z; y = z; z = w; }
-    assert(a == 1); return ModInt(y);
+    assert(a == 1U); return ModInt(y);
   }
   ModInt operator+() const { return *this; }
-  ModInt operator-() const { ModInt a; a.x = x ? (M - x) : 0; return a; }
+  ModInt operator-() const { ModInt a; a.x = x ? (M - x) : 0U; return a; }
   ModInt operator+(const ModInt &a) const { return (ModInt(*this) += a); }
   ModInt operator-(const ModInt &a) const { return (ModInt(*this) -= a); }
   ModInt operator*(const ModInt &a) const { return (ModInt(*this) *= a); }
