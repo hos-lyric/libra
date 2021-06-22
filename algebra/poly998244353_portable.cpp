c9dcf04fb6627bf9f9ecf318222b8639399ffec7
hos <hos@hos.ac>
2021-06-22 12:27:52 +0900

    Update poly998244353_portable

--- algebra/poly998244353_portable.cpp
+++ algebra/poly998244353_portable.cpp
@@ -2,7 +2,7 @@
 template <unsigned M_> struct ModInt {
   static constexpr unsigned M = M_;
   unsigned x;
-  constexpr ModInt() : x(0) {}
+  constexpr ModInt() : x(0U) {}
   constexpr ModInt(unsigned x_) : x(x_ % M) {}
   constexpr ModInt(unsigned long long x_) : x(x_ % M) {}
   constexpr ModInt(int x_) : x(((x_ %= static_cast<int>(M)) < 0) ? (x_ + static_cast<int>(M)) : x_) {}
@@ -13,15 +13,15 @@ template <unsigned M_> struct ModInt {
   ModInt &operator/=(const ModInt &a) { return (*this *= a.inv()); }
   ModInt pow(long long e) const {
     if (e < 0) return inv().pow(-e);
-    ModInt a = *this, b = 1; for (; e; e >>= 1) { if (e & 1) b *= a; a *= a; } return b;
+    ModInt a = *this, b = 1U; for (; e; e >>= 1) { if (e & 1) b *= a; a *= a; } return b;
   }
   ModInt inv() const {
     unsigned a = M, b = x; int y = 0, z = 1;
     for (; b; ) { const unsigned q = a / b; const unsigned c = a - q * b; a = b; b = c; const int w = y - static_cast<int>(q) * z; y = z; z = w; }
-    assert(a == 1); return ModInt(y);
+    assert(a == 1U); return ModInt(y);
   }
   ModInt operator+() const { return *this; }
-  ModInt operator-() const { ModInt a; a.x = x ? (M - x) : 0; return a; }
+  ModInt operator-() const { ModInt a; a.x = x ? (M - x) : 0U; return a; }
   ModInt operator+(const ModInt &a) const { return (ModInt(*this) += a); }
   ModInt operator-(const ModInt &a) const { return (ModInt(*this) -= a); }
   ModInt operator*(const ModInt &a) const { return (ModInt(*this) *= a); }
@@ -44,8 +44,8 @@ constexpr int FFT_MAX = 23;
 using Mint = ModInt<MO>;
 constexpr Mint FFT_ROOTS[FFT_MAX + 1] = {1U, 998244352U, 911660635U, 372528824U, 929031873U, 452798380U, 922799308U, 781712469U, 476477967U, 166035806U, 258648936U, 584193783U, 63912897U, 350007156U, 666702199U, 968855178U, 629671588U, 24514907U, 996173970U, 363395222U, 565042129U, 733596141U, 267099868U, 15311432U};
 constexpr Mint INV_FFT_ROOTS[FFT_MAX + 1] = {1U, 998244352U, 86583718U, 509520358U, 337190230U, 87557064U, 609441965U, 135236158U, 304459705U, 685443576U, 381598368U, 335559352U, 129292727U, 358024708U, 814576206U, 708402881U, 283043518U, 3707709U, 121392023U, 704923114U, 950391366U, 428961804U, 382752275U, 469870224U};
-constexpr Mint FFT_RATIOS[FFT_MAX - 1] = {911660635U, 509520358U, 369330050U, 332049552U, 983190778U, 123842337U, 238493703U, 975955924U, 603855026U, 856644456U, 131300601U, 842657263U, 730768835U, 942482514U, 806263778U, 151565301U, 510815449U, 503497456U, 743006876U, 741047443U, 56250497U, 867605899U};
-constexpr Mint INV_FFT_RATIOS[FFT_MAX - 1] = {86583718U, 372528824U, 373294451U, 645684063U, 112220581U, 692852209U, 155456985U, 797128860U, 90816748U, 860285882U, 927414960U, 354738543U, 109331171U, 293255632U, 535113200U, 308540755U, 121186627U, 608385704U, 438932459U, 359477183U, 824071951U, 103369235U};
+constexpr Mint FFT_RATIOS[FFT_MAX] = {911660635U, 509520358U, 369330050U, 332049552U, 983190778U, 123842337U, 238493703U, 975955924U, 603855026U, 856644456U, 131300601U, 842657263U, 730768835U, 942482514U, 806263778U, 151565301U, 510815449U, 503497456U, 743006876U, 741047443U, 56250497U, 867605899U};
+constexpr Mint INV_FFT_RATIOS[FFT_MAX] = {86583718U, 372528824U, 373294451U, 645684063U, 112220581U, 692852209U, 155456985U, 797128860U, 90816748U, 860285882U, 927414960U, 354738543U, 109331171U, 293255632U, 535113200U, 308540755U, 121186627U, 608385704U, 438932459U, 359477183U, 824071951U, 103369235U};
 
 // as[rev(i)] <- \sum_j \zeta^(ij) as[j]
 void fft(Mint *as, int n) {
@@ -59,7 +59,7 @@ void fft(Mint *as, int n) {
     }
   }
   if (m >>= 1) {
-    Mint prod = 1;
+    Mint prod = 1U;
     for (int h = 0, i0 = 0; i0 < n; i0 += (m << 1)) {
       for (int i = i0; i < i0 + m; ++i) {
         const unsigned x = (prod * as[i + m]).x;  // < MO
@@ -71,7 +71,7 @@ void fft(Mint *as, int n) {
   }
   for (; m; ) {
     if (m >>= 1) {
-      Mint prod = 1;
+      Mint prod = 1U;
       for (int h = 0, i0 = 0; i0 < n; i0 += (m << 1)) {
         for (int i = i0; i < i0 + m; ++i) {
           const unsigned x = (prod * as[i + m]).x;  // < MO
@@ -82,7 +82,7 @@ void fft(Mint *as, int n) {
       }
     }
     if (m >>= 1) {
-      Mint prod = 1;
+      Mint prod = 1U;
       for (int h = 0, i0 = 0; i0 < n; i0 += (m << 1)) {
         for (int i = i0; i < i0 + m; ++i) {
           const unsigned x = (prod * as[i + m]).x;  // < MO
@@ -105,7 +105,7 @@ void invFft(Mint *as, int n) {
   assert(!(n & (n - 1))); assert(1 <= n); assert(n <= 1 << FFT_MAX);
   int m = 1;
   if (m < n >> 1) {
-    Mint prod = 1;
+    Mint prod = 1U;
     for (int h = 0, i0 = 0; i0 < n; i0 += (m << 1)) {
       for (int i = i0; i < i0 + m; ++i) {
         const unsigned long long y = as[i].x + MO - as[i + m].x;  // < 2 MO
@@ -117,7 +117,7 @@ void invFft(Mint *as, int n) {
     m <<= 1;
   }
   for (; m < n >> 1; m <<= 1) {
-    Mint prod = 1;
+    Mint prod = 1U;
     for (int h = 0, i0 = 0; i0 < n; i0 += (m << 1)) {
       for (int i = i0; i < i0 + (m >> 1); ++i) {
         const unsigned long long y = as[i].x + MO2 - as[i + m].x;  // < 4 MO
@@ -161,6 +161,17 @@ vector<Mint> convolve(vector<Mint> as, vector<Mint> bs) {
   as.resize(n); fft(as);
   bs.resize(n); fft(bs);
   for (int i = 0; i < n; ++i) as[i] *= bs[i];
+  invFft(as);
+  as.resize(len);
+  return as;
+}
+vector<Mint> square(vector<Mint> as) {
+  if (as.empty()) return {};
+  const int len = as.size() + as.size() - 1;
+  int n = 1;
+  for (; n < len; n <<= 1) {}
+  as.resize(n); fft(as);
+  for (int i = 0; i < n; ++i) as[i] *= as[i];
   invFft(as);
   as.resize(len);
   return as;
