d2b495f33760dcc73e01e7782621593e0bd4291d
hos <hos@hos.ac>
2023-03-05 14:08:05 +0900

    Add template

---
+++ tem_unittest.cpp
@@ -0,0 +1,16 @@
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+using std::cerr;
+using std::endl;
+
+void unittest() {
+}
+
+int main() {
+  unittest(); cerr << "PASSED unittest" << endl;
+  return 0;
+}
