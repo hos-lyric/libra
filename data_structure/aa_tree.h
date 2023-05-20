60b8b56ecae5860f81d75de28510d94336f5dad9
hos <hos@hos.ac>
2023-05-21 00:20:44 +0900

    Add AA tree

---
+++ data_structure/aa_tree.h
@@ -0,0 +1,143 @@
+#ifndef LIBRA_DATA_STRUCTURE_AA_TREE_H_
+#define LIBRA_DATA_STRUCTURE_AA_TREE_H_
+
+#include <assert.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// leaf node: uncut interval
+// internal node: concatnation of intervals for l and r (constitutes AA tree)
+// T: monoid representing information of an interval.
+//   T()  should return the identity.
+//   T(S s)  should represent a single element of the array.
+//   T::push(T &l, T &r)  should push the lazy update.
+//   T::pull(const T &l, const T &r)  should pull two intervals.
+// (in addition to T for SegmentTreeRange)
+//   T::sz  should represent the length of the interval.
+//   T::init(SignedInteger sz_)  should initialize an interval (for T()).
+// Usage: u = AATree<T>::cut(u, a);
+template <class T> struct AATree {
+  using Size = decltype(T().sz);
+  AATree *l, *r;
+  int lev;
+  T t;
+
+  // Creates [0, sz).
+  AATree(Size sz) : l(nullptr), r(nullptr), lev(0), t() {
+    t.init(sz);
+  }
+
+  static void free(AATree *u) {
+    if (!u) return;
+    free(u->l);
+    free(u->r);
+    delete u;
+  }
+
+  inline Size sz() const {
+    return t.sz;
+  }
+  inline void push() {
+    t.push(l->t, r->t);
+  }
+  inline void pull() {
+    t.pull(l->t, r->t);
+  }
+
+  //   v---u          v---u   //
+  //  / \   \   ->   /   / \  //
+  // o   o   o      o   o   o //
+  static AATree *rotSkew(AATree *u) {
+    if (!(u->l && u->lev == u->l->lev)) return u;
+    AATree *v = u->l;
+    u->l = v->r;
+    u->pull();
+    v->r = u;
+    v->pull();
+    return v;
+  }
+  //                      v   //
+  //                     / \  //
+  //   u---v---o        u   o //
+  //  /   /       ->   / \    //
+  // o   o            o   o   //
+  static AATree *rotSplit(AATree *u) {
+    if (!(u->r && u->r->r && u->lev == u->r->r->lev)) return u;
+    AATree *v = u->r;
+    u->r = v->l;
+    u->pull();
+    v->l = u;
+    ++v->lev;
+    v->pull();
+    return v;
+  }
+
+  // Cuts at a.
+  static AATree *cut(AATree *u, Size a) {
+    if (a <= 0 || u->sz() <= a) return u;
+    if (!u->lev) {
+      u->l = new AATree(a);
+      u->r = new AATree(u->sz() - a);
+      u->lev = 1;
+      u->push();
+      return u;
+    }
+    u->push();
+    if (a <= u->l->sz()) {
+      u->l = cut(u->l, a);
+      u->pull();
+      return rotSplit(rotSkew(u));
+    } else {
+      u->r = cut(u->r, a - u->l->sz());
+      u->pull();
+      return rotSplit(rotSkew(u));
+    }
+  }
+
+  // Applies T::f(args...) to [a, b).
+  template <class F, class... Args> void ch(Size a, Size b, F f, Args &&... args) {
+    if (b <= 0 || sz() <= a) return;
+    if (a <= 0 && sz() <= b) {
+      (t.*f)(args...);
+      return;
+    }
+    push();
+    l->ch(a, b, f, args...);
+    r->ch(a - l->sz(), b - l->sz(), f, args...);
+    pull();
+  }
+
+  // Calculates the product for [a, b).
+  T get(Size a, Size b) {
+    if (b <= 0 || sz() <= a) return T();
+    if (a <= 0 && sz() <= b) return t;
+    push();
+    const T prodL = l->get(a, b);
+    const T prodR = r->get(a - l->sz(), b - l->sz());
+    T prod;
+    prod.pull(prodL, prodR);
+    return prod;
+  }
+
+  // Calculates T::f(args...) of a monoid type for [a, b).
+  //   op(-, -)  should calculate the product.
+  //   e()  should return the identity.
+  template <class Op, class E, class F, class... Args>
+#if __cplusplus >= 201402L
+  auto
+#else
+  decltype((std::declval<T>().*F())())
+#endif
+  get(Size a, Size b, Op op, E e, F f, Args &&... args) {
+    if (b <= 0 || sz() <= a) return e();
+    if (a <= 0 && sz() <= b) return (t.*f)(args...);
+    push();
+    const auto prodL = l->get(a, b, op, e, f, args...);
+    const auto prodR = r->get(a - l->sz(), b - l->sz(), op, e, f, args...);
+    return op(prodL, prodR);
+  }
+
+  // TODO: findRight, findLeft
+};
+////////////////////////////////////////////////////////////////////////////////
+
+#endif  // LIBRA_DATA_STRUCTURE_AA_TREE_H_
---
+++ data_structure/aa_tree_test.cpp
@@ -0,0 +1,113 @@
+#include "aa_tree.h"
+
+#include <assert.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using std::cerr;
+using std::endl;
+using std::swap;
+using std::vector;
+
+////////////////////////////////////////////////////////////////////////////////
+// !!!Size must be a signed integer type!!!
+// range add, range sum
+template <class Size_, class T> struct NodeSum {
+  using Size = Size_;
+  Size sz;
+  T sum, lz;
+  NodeSum() : sz(0), sum(0), lz(0) {}
+  void init(Size sz_) {
+    sz = sz_;
+  }
+  void push(NodeSum &l, NodeSum &r) {
+    if (lz) {
+      l.add(lz);
+      r.add(lz);
+      lz = 0;
+    }
+  }
+  void pull(const NodeSum &l, const NodeSum &r) {
+    sz = l.sz + r.sz;
+    sum = l.sum + r.sum;
+  }
+  void add(const T &val) {
+    sum += static_cast<T>(sz) * val;
+    lz += val;
+  }
+  T getSum() const {
+    return sum;
+  }
+};
+////////////////////////////////////////////////////////////////////////////////
+
+unsigned xrand() {
+  static unsigned x = 314159265, y = 358979323, z = 846264338, w = 327950288;
+  unsigned t = x ^ x << 11; x = y; y = z; z = w; return w = w ^ w >> 19 ^ t ^ t >> 8;
+}
+
+void unittest() {
+  constexpr int NUM_CASES = 100;
+  constexpr int MAX_N = 100;
+  constexpr int NUM_QUERIES = 1000;
+  for (int caseId = 0; caseId < NUM_CASES; ++caseId) {
+    const int n = 1 + xrand() % MAX_N;
+    vector<long long> xs(n + 1, 0);
+    for (int i = 1; i <= n; ++i) {
+      xs[i] = static_cast<long long>(xrand()) << 20;
+      xs[i] |= xrand();
+    }
+    std::sort(xs.begin(), xs.end());
+    vector<int> is{0, n};
+    vector<unsigned> brt(n, 0);
+    using Node = NodeSum<long long, unsigned>;
+    using AA = AATree<Node>;
+    AA *seg = new AA(xs[n]);
+    for (int q = 0; q < NUM_QUERIES; ++q) {
+      switch (xrand() % 3) {
+        case 0: {
+          const int i = xrand() % (n + 1);
+          is.push_back(i);
+          std::sort(is.begin(), is.end());
+          is.erase(std::unique(is.begin(), is.end()), is.end());
+          seg = AA::cut(seg, xs[i]);
+        } break;
+        case 1: {
+          int a = is[xrand() % is.size()];
+          int b = is[xrand() % is.size()];
+          if (a > b) swap(a, b);
+          const unsigned val = xrand();
+          for (int i = a; i < b; ++i) {
+            brt[i] += val;
+          }
+          seg->ch(xs[a], xs[b], &Node::add, val);
+        } break;
+        case 2: {
+          int a = is[xrand() % is.size()];
+          int b = is[xrand() % is.size()];
+          if (a > b) swap(a, b);
+          unsigned expected = 0;
+          for (int i = a; i < b; ++i) {
+            expected += static_cast<unsigned>(xs[i + 1] - xs[i]) * brt[i];
+          }
+          const unsigned actual0 = seg->get(xs[a], xs[b]).sum;
+          const unsigned actual1 = seg->get(
+              xs[a], xs[b],
+              [&](unsigned x, unsigned y) -> unsigned { return x + y; },
+              [&]() -> unsigned { return 0; },
+              &Node::getSum);
+          assert(expected == actual0);
+          assert(expected == actual1);
+        } break;
+        default: assert(false);
+      }
+    }
+    AA::free(seg);
+  }
+}
+
+int main() {
+  unittest(); cerr << "PASSED unittest" << endl;
+  return 0;
+}
