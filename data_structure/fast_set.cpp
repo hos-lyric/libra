29f675228f91efa2d18f0a3a328d4317094beb1b
hos <hos@hos.ac>
2023-09-06 08:38:00 +0900

    Fix n=0

--- data_structure/fast_set.cpp
+++ data_structure/fast_set.cpp
@@ -1,20 +1,19 @@
 #include <assert.h>
-#include <stdio.h>
 #include <vector>
 
 using std::vector;
 
-// [0, n), 1 <= n <= 2^(6D)
+// [0, n), 0 <= n <= 2^(6D)
 template <int D> struct Set {
   int n;
   vector<unsigned long long> a[D];
-  Set() {}
-  Set(int n_) : n(n_) {
+  explicit Set(int n_ = 0) : n(n_) {
     static_assert(1 <= D && D <= 6, "Set: 1 <= D <= 6 must hold");
-    assert(1 <= n_ && n_ <= 1LL << (6 * D));
+    assert(0 <= n); assert(n <= 1LL << (6 * D));
+    int m = n ? n : 1;
     for (int d = 0; d < D; ++d) {
-      n_ = (n_ + 63) >> 6;
-      a[d].assign(n_, 0);
+      m = (m + 63) >> 6;
+      a[d].assign(m, 0);
     }
   }
   bool empty() const {
@@ -41,7 +40,7 @@ template <int D> struct Set {
   int next(int x) const {
     for (int d = 0; d < D; ++d) {
       const int q = x >> 6, r = x & 63;
-      if (static_cast<size_t>(q) >= a[d].size()) break;
+      if (static_cast<unsigned>(q) >= a[d].size()) break;
       const unsigned long long upper = a[d][q] >> r;
       if (upper) {
         x += __builtin_ctzll(upper);
@@ -69,46 +68,61 @@ template <int D> struct Set {
   }
 };
 
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+using std::cerr;
+using std::endl;
+
 void unittest() {
-  Set<5> s(123456789);
-  assert(s.empty());
-  s.insert(314159);
-  s.insert(141592);
-  assert(!s.empty());
-  s.erase(314159);
-  s.erase(141592);
-  assert(s.empty());
-  s.insert(653589);
-  s.insert(793238);
-  s.insert(462643);
-  s.erase(653589);
-  s.insert(383279);
-  s.insert(502884);
-  s.erase(502884);
-  s.insert(383289);
-  s.insert(462743);
-  s.insert(794238);
-  s.erase(462643);
-  // {383279, 383289, 462743, 793238, 794238}
-  assert(s.next(400000) == 462743);
-  assert(s.next(462743) == 462743);
-  assert(s.next(462744) == 793238);
-  assert(s.next(800000) == 123456789);
-  assert(s.next(123456788) == 123456789);
-  assert(s.prev(800000) == 794238);
-  assert(s.prev(383289) == 383289);
-  assert(s.prev(383288) == 383279);
-  assert(s.prev(300000) == -1);
-  assert(s.prev(0) == -1);
+  {
+    Set<1> s;
+    assert(s.empty());
+    assert(s.next(0) == 0);
+    assert(s.next(1) == 0);
+    assert(s.prev(0) == -1);
+    assert(s.prev(1) == -1);
+  }
+  {
+    Set<5> s(123456789);
+    assert(s.empty());
+    s.insert(314159);
+    s.insert(141592);
+    assert(!s.empty());
+    s.erase(314159);
+    s.erase(141592);
+    assert(s.empty());
+    s.insert(653589);
+    s.insert(793238);
+    s.insert(462643);
+    s.erase(653589);
+    s.insert(383279);
+    s.insert(502884);
+    s.erase(502884);
+    s.insert(383289);
+    s.insert(462743);
+    s.insert(794238);
+    s.erase(462643);
+    // {383279, 383289, 462743, 793238, 794238}
+    assert(s.next(400000) == 462743);
+    assert(s.next(462743) == 462743);
+    assert(s.next(462744) == 793238);
+    assert(s.next(800000) == 123456789);
+    assert(s.next(123456788) == 123456789);
+    assert(s.prev(800000) == 794238);
+    assert(s.prev(383289) == 383289);
+    assert(s.prev(383288) == 383279);
+    assert(s.prev(300000) == -1);
+    assert(s.prev(0) == -1);
+  }
 }
 
 
 // https://judge.yosupo.jp/problem/predecessor_problem
-char T[10000010];
-
-int main() {
-  // unittest();
-  
+#include <stdio.h>
+char T[10'000'010];
+void yosupo_predecessor_problem() {
   int N, Q;
   for (; ~scanf("%d%d", &N, &Q); ) {
     scanf("%s", T);
@@ -144,5 +158,10 @@ int main() {
       }
     }
   }
+}
+
+int main() {
+  unittest(); cerr << "PASSED unittest" << endl;
+  // yosupo_predecessor_problem();
   return 0;
 }
