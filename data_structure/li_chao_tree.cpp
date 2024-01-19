3fb630d143a79f2a0eace4ae69258e53efca3518
hos <hos@hos.ac>
2024-01-19 21:59:24 +0900

    Add Li Chao tree for line segments in C++

--- data_structure/li_chao_tree.cpp
+++ data_structure/li_chao_tree.cpp
@@ -2,14 +2,13 @@
 #include <utility>
 #include <vector>
 
+using std::pair;
 using std::swap;
 using std::vector;
 
-// TODO: subsegment
-
 // class Func = TX -> TY
 //   Uses TY::operator< for comparison.
-//   For any f, g: Func, [f < g] must be monotone.
+//   For any f, g: Func, [f < g] must be monotone on [XL, XR).
 
 template <class Func> struct MinLiChaoTree {
   using TX = typename Func::TX;
@@ -64,7 +63,7 @@ template <class Func> struct MinLiChaoTree {
     }
     return minY;
   }
-};
+};  // MinLiChaoTree
 
 template <class Func> struct MaxLiChaoTree {
   using TX = typename Func::TX;
@@ -119,13 +118,160 @@ template <class Func> struct MaxLiChaoTree {
     }
     return maxY;
   }
-};
+};  // MaxLiChaoTree
+
+template <class Func> struct MinLiChaoTreeSegment {
+  using TX = typename Func::TX;
+  using TY = typename Func::TY;
+  struct Node {
+    int l, r;
+    bool has;
+    Func f;
+  };
+  vector<Node> nodes;
+
+  const TX XL, XR;
+  int rt;
+  MinLiChaoTreeSegment() : XL(0), XR(0), rt(-1) {}
+  // [XL, XR)
+  MinLiChaoTreeSegment(TX XL_, TX XR_) : XL(XL_), XR(XR_), rt(-1) {}
+  // Add f to the whole [XL, XR).
+  void add(const Func &f) {
+    addSub(&rt, XL, XR, f);
+  }
+  // Add f to [xA, xB).
+  void add(TX xA, TX xB, const Func &f) {
+    assert(XL <= xA); assert(xA <= xB); assert(xB <= XR);
+    rt = addRec(rt, XL, XR, xA, xB, f);
+  }
+  // Get min at x.
+  // Returns (true, min at x) or (false, TY())
+  pair<bool, TY> operator()(TX x) const {
+    assert(XL <= x); assert(x < XR);
+    pair<bool, TY> minY(false, TY());
+    int u = rt;
+    for (TX xL = XL, xR = XR; ~u; ) {
+      if (nodes[u].has) {
+        const TY y = nodes[u].f(x);
+        if (!minY.first || y < minY.second) minY = std::make_pair(true, y);
+      }
+      const TX xMid = xL + (xR - xL) / 2;
+      if (x < xMid) {
+        u = nodes[u].l; xR = xMid;
+      } else {
+        u = nodes[u].r; xL = xMid;
+      }
+    }
+    return minY;
+  }
+
+  void addSub(int *u, TX xL, TX xR, Func f) {
+    for (; ; ) {
+      if (!~*u) { *u = nodes.size(); nodes.push_back({-1, -1, true, f}); return; }
+      if (!nodes[*u].has) { nodes[*u].has = true; nodes[*u].f = f; return; }
+      const TX xMid = xL + (xR - xL) / 2;
+      if (f(xMid) < nodes[*u].f(xMid)) swap(nodes[*u].f, f);
+      if (xL + 1 == xR) return;
+      if (f(xL) < nodes[*u].f(xL)) {
+        u = &nodes[*u].l; xR = xMid;
+      } else if (f(xR - 1) < nodes[*u].f(xR - 1)) {
+        u = &nodes[*u].r; xL = xMid;
+      } else {
+        return;
+      }
+    }
+  }
+  int addRec(int u, TX xL, TX xR, TX xA, TX xB, const Func &f) {
+    if (xB <= xL || xR <= xA) return u;
+    if (xA <= xL && xR <= xB) { int v = u; addSub(&v, xL, xR, f); return v; }
+    if (~u && nodes[u].has && nodes[u].f(xL) < f(xL) && nodes[u].f(xR - 1) < f(xR - 1)) return u;
+    if (!~u) { u = nodes.size(); nodes.push_back({-1, -1, false, Func()}); }
+    const TX xMid = xL + (xR - xL) / 2;
+    const int uL = addRec(nodes[u].l, xL, xMid, xA, xB, f); nodes[u].l = uL;
+    const int uR = addRec(nodes[u].r, xMid, xR, xA, xB, f); nodes[u].r = uR;
+    return u;
+  }
+};  // MinLiChaoTreeSegment
+
+template <class Func> struct MaxLiChaoTreeSegment {
+  using TX = typename Func::TX;
+  using TY = typename Func::TY;
+  struct Node {
+    int l, r;
+    bool has;
+    Func f;
+  };
+  vector<Node> nodes;
+
+  const TX XL, XR;
+  int rt;
+  MaxLiChaoTreeSegment() : XL(0), XR(0), rt(-1) {}
+  // [XL, XR)
+  MaxLiChaoTreeSegment(TX XL_, TX XR_) : XL(XL_), XR(XR_), rt(-1) {}
+  // Add f to the whole [XL, XR).
+  void add(const Func &f) {
+    addSub(&rt, XL, XR, f);
+  }
+  // Add f to [xA, xB).
+  void add(TX xA, TX xB, const Func &f) {
+    assert(XL <= xA); assert(xA <= xB); assert(xB <= XR);
+    rt = addRec(rt, XL, XR, xA, xB, f);
+  }
+  // Get max at x.
+  // Returns (true, max at x) or (false, TY())
+  pair<bool, TY> operator()(TX x) const {
+    assert(XL <= x); assert(x < XR);
+    pair<bool, TY> maxY(false, TY());
+    int u = rt;
+    for (TX xL = XL, xR = XR; ~u; ) {
+      if (nodes[u].has) {
+        const TY y = nodes[u].f(x);
+        if (!maxY.first || maxY.second < y) maxY = std::make_pair(true, y);
+      }
+      const TX xMid = xL + (xR - xL) / 2;
+      if (x < xMid) {
+        u = nodes[u].l; xR = xMid;
+      } else {
+        u = nodes[u].r; xL = xMid;
+      }
+    }
+    return maxY;
+  }
+
+  void addSub(int *u, TX xL, TX xR, Func f) {
+    for (; ; ) {
+      if (!~*u) { *u = nodes.size(); nodes.push_back({-1, -1, true, f}); return; }
+      if (!nodes[*u].has) { nodes[*u].has = true; nodes[*u].f = f; return; }
+      const TX xMid = xL + (xR - xL) / 2;
+      if (nodes[*u].f(xMid) < f(xMid)) swap(nodes[*u].f, f);
+      if (xL + 1 == xR) return;
+      if (nodes[*u].f(xL) < f(xL)) {
+        u = &nodes[*u].l; xR = xMid;
+      } else if (nodes[*u].f(xR - 1) < f(xR - 1)) {
+        u = &nodes[*u].r; xL = xMid;
+      } else {
+        return;
+      }
+    }
+  }
+  int addRec(int u, TX xL, TX xR, TX xA, TX xB, const Func &f) {
+    if (xB <= xL || xR <= xA) return u;
+    if (xA <= xL && xR <= xB) { int v = u; addSub(&v, xL, xR, f); return v; }
+    if (~u && nodes[u].has && f(xL) < nodes[u].f(xL) && f(xR - 1) < nodes[u].f(xR - 1)) return u;
+    if (!~u) { u = nodes.size(); nodes.push_back({-1, -1, false, Func()}); }
+    const TX xMid = xL + (xR - xL) / 2;
+    const int uL = addRec(nodes[u].l, xL, xMid, xA, xB, f); nodes[u].l = uL;
+    const int uR = addRec(nodes[u].r, xMid, xR, xA, xB, f); nodes[u].r = uR;
+    return u;
+  }
+};  // MaxLiChaoTreeSegment
 
 // y = p x + q
 struct Line {
   using TX = long long;
   using TY = long long;
   long long p, q;
+  Line() {}
   Line(long long p_, long long q_) : p(p_), q(q_) {}
   TY operator()(TX x) const {
     return p * x + q;
@@ -224,8 +370,142 @@ void unittest() {
             if (mn > line(x)) mn = line(x);
             if (mx < line(x)) mx = line(x);
           }
-          assert(mn == minLct(x));
-          assert(mx == maxLct(x));
+          assert(minLct(x) == mn);
+          assert(maxLct(x) == mx);
+        }
+      }
+    }
+  }
+}
+
+// y = p x + q
+struct LineTester {
+  using TX = long long;
+  using TY = long long;
+  bool has;
+  long long p, q;
+  LineTester() : has(false), p(0), q(0) {}
+  LineTester(long long p_, long long q_) : has(true), p(p_), q(q_) {}
+  TY operator()(TX x) const {
+    assert(has);
+    return p * x + q;
+  }
+};
+
+void unittest_Segment() {
+  using Pair = pair<bool, LineTester::TY>;
+  {
+    MinLiChaoTreeSegment<LineTester> lct(-10, 10);
+    lct.add(-6, 6, LineTester(1, 3));
+    assert(lct(-7) == Pair(false, 0));
+    assert(lct(-6) == Pair(true, -3));
+    assert(lct(5) == Pair(true, 8));
+    assert(lct(6) == Pair(false, 0));
+    lct.add(-4, 0, LineTester(2, 6));
+    assert(lct(-5) == Pair(true, -2));
+    assert(lct(-4) == Pair(true, -2));
+    assert(lct(-3) == Pair(true, 0));
+    assert(lct(-2) == Pair(true, 1));
+    assert(lct(-1) == Pair(true, 2));
+    assert(lct(0) == Pair(true, 3));
+    lct.add(-2, 2, LineTester(1, 4));
+    assert(lct(-3) == Pair(true, 0));
+    assert(lct(-2) == Pair(true, 1));
+    assert(lct(-1) == Pair(true, 2));
+    assert(lct(0) == Pair(true, 3));
+    assert(lct(1) == Pair(true, 4));
+    assert(lct(2) == Pair(true, 5));
+    lct.add(-2, 1, LineTester(5, 9));
+    assert(lct(-3) == Pair(true, 0));
+    assert(lct(-2) == Pair(true, -1));
+    assert(lct(-1) == Pair(true, 2));
+    assert(lct(0) == Pair(true, 3));
+    assert(lct(1) == Pair(true, 4));
+  }
+  {
+    MaxLiChaoTreeSegment<LineTester> lct(-10, 10);
+    lct.add(-6, 6, LineTester(1, 3));
+    assert(lct(-7) == Pair(false, 0));
+    assert(lct(-6) == Pair(true, -3));
+    assert(lct(5) == Pair(true, 8));
+    assert(lct(6) == Pair(false, 0));
+    lct.add(-4, 0, LineTester(2, 6));
+    assert(lct(-5) == Pair(true, -2));
+    assert(lct(-4) == Pair(true, -1));
+    assert(lct(-3) == Pair(true, 0));
+    assert(lct(-2) == Pair(true, 2));
+    assert(lct(-1) == Pair(true, 4));
+    assert(lct(0) == Pair(true, 3));
+    lct.add(-2, 2, LineTester(1, 4));
+    assert(lct(-3) == Pair(true, 0));
+    assert(lct(-2) == Pair(true, 2));
+    assert(lct(-1) == Pair(true, 4));
+    assert(lct(0) == Pair(true, 4));
+    assert(lct(1) == Pair(true, 5));
+    assert(lct(2) == Pair(true, 5));
+    lct.add(-2, 1, LineTester(5, 9));
+    assert(lct(-3) == Pair(true, 0));
+    assert(lct(-2) == Pair(true, 2));
+    assert(lct(-1) == Pair(true, 4));
+    assert(lct(0) == Pair(true, 9));
+    assert(lct(1) == Pair(true, 5));
+  }
+  {
+    constexpr int NUM_CASES = 1000;
+    constexpr int NUM_QUERIES = 1000;
+    constexpr long long LIM = -1'000'000'000;
+    for (int caseId = 0; caseId < NUM_CASES; ++caseId) {
+      long long L, R;
+      for (; ; ) {
+        L = -LIM + static_cast<long long>(xrand()) % (2 * LIM + 1);
+        R = -LIM + static_cast<long long>(xrand()) % (2 * LIM + 1);
+        if (L < R) break;
+      }
+      vector<pair<pair<long long, long long>, LineTester>> lineSegments;
+      MinLiChaoTreeSegment<LineTester> minLct(L, R);
+      MaxLiChaoTreeSegment<LineTester> maxLct(L, R);
+      for (int queryId = 0; queryId < NUM_QUERIES; ++queryId) {
+        if (xrand() & 1) {
+          long long a, b;
+          if (xrand() % 100 == 0) {
+            a = L;
+            b = R;
+          } else {
+            for (; ; ) {
+              a = L + static_cast<long long>(xrand()) % (R - L);
+              b = L + static_cast<long long>(xrand()) % (R - L) + 1;
+              if (a < b) break;
+            }
+          }
+          const long long p = -LIM + static_cast<long long>(xrand()) % (2 * LIM + 1);
+          const long long q = -LIM + static_cast<long long>(xrand()) % (2 * LIM + 1);
+          const LineTester line(p, q);
+          lineSegments.emplace_back(std::make_pair(a, b), line);
+          if (a == L && b == R) {
+            minLct.add(line);
+            maxLct.add(line);
+          } else {
+            minLct.add(a, b, line);
+            maxLct.add(a, b, line);
+          }
+        } else {
+          const long long x = L + static_cast<long long>(xrand()) % (R - L);
+          bool has = false;
+          long long mn = 0, mx = 0;
+          for (const auto &lineSegment : lineSegments) {
+            if (lineSegment.first.first <= x && x < lineSegment.first.second) {
+              const LineTester &line = lineSegment.second;
+              if (has) {
+                if (mn > line(x)) mn = line(x);
+                if (mx < line(x)) mx = line(x);
+              } else {
+                has = true;
+                mn = mx = line(x);
+              }
+            }
+          }
+          assert(minLct(x) == Pair(has, mn));
+          assert(maxLct(x) == Pair(has, mx));
         }
       }
     }
@@ -296,9 +576,82 @@ void yosupoMax_line_add_get_min() {
   }
 }
 
+// https://judge.yosupo.jp/problem/segment_add_get_min
+void yosupoMin_segment_add_get_min() {
+  constexpr long long LIM_X = 1'000'000'000LL;
+  int N, Q;
+  for (; ~scanf("%d%d", &N, &Q); ) {
+    MinLiChaoTreeSegment<Line> lct(-LIM_X, LIM_X + 1);
+    for (int i = 0; i < N; ++i) {
+      long long l, r, a, b;
+      scanf("%lld%lld%lld%lld", &l, &r, &a, &b);
+      lct.add(l, r, Line(a, b));
+    }
+    for (int q = 0; q < Q; ++q) {
+      int typ;
+      scanf("%d", &typ);
+      switch (typ) {
+        case 0: {
+          long long l, r, a, b;
+          scanf("%lld%lld%lld%lld", &l, &r, &a, &b);
+          lct.add(l, r, Line(a, b));
+        } break;
+        case 1: {
+          long long p;
+          scanf("%lld", &p);
+          const pair<bool, long long> ans = lct(p);
+          if (ans.first) {
+            printf("%lld\n", ans.second);
+          } else {
+            puts("INFINITY");
+          }
+        } break;
+        default: assert(false);
+      }
+    }
+  }
+}
+void yosupoMax_segment_add_get_min() {
+  constexpr long long LIM_X = 1'000'000'000LL;
+  int N, Q;
+  for (; ~scanf("%d%d", &N, &Q); ) {
+    MaxLiChaoTreeSegment<Line> lct(-LIM_X, LIM_X + 1);
+    for (int i = 0; i < N; ++i) {
+      long long l, r, a, b;
+      scanf("%lld%lld%lld%lld", &l, &r, &a, &b);
+      lct.add(l, r, Line(-a, -b));
+    }
+    for (int q = 0; q < Q; ++q) {
+      int typ;
+      scanf("%d", &typ);
+      switch (typ) {
+        case 0: {
+          long long l, r, a, b;
+          scanf("%lld%lld%lld%lld", &l, &r, &a, &b);
+          lct.add(l, r, Line(-a, -b));
+        } break;
+        case 1: {
+          long long p;
+          scanf("%lld", &p);
+          const pair<bool, long long> ans = lct(p);
+          if (ans.first) {
+            printf("%lld\n", -ans.second);
+          } else {
+            puts("INFINITY");
+          }
+        } break;
+        default: assert(false);
+      }
+    }
+  }
+}
+
 int main() {
   unittest(); cerr << "PASSED unittest" << endl;
+  unittest_Segment(); cerr << "PASSED unittest_Segment" << endl;
   // yosupoMin_line_add_get_min();
   // yosupoMax_line_add_get_min();
+  // yosupoMin_segment_add_get_min();
+  // yosupoMax_segment_add_get_min();
   return 0;
 }
