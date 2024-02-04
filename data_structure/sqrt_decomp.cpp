5c9dc9b71ed5fb196ec03b767f190790e1e4c954
hos <hos@hos.ac>
2024-02-05 01:33:37 +0900

    Add PointAddRangeSum

--- data_structure/sqrt_decomp.cpp
+++ data_structure/sqrt_decomp.cpp
@@ -4,7 +4,52 @@
 #include <vector>
 
 using std::max;
+using std::min;
 using std::vector;
+
+// add val to a: O(sqrt(n))
+// get sum of [a, b): O(1)
+template <class T> struct PointAddRangeSum {
+  int n, l, e;
+  vector<T> giant, baby;
+  PointAddRangeSum() {}
+  PointAddRangeSum(int n_) : n(n_) {
+    for (e = 0; 1 << (2 * e) < n; ++e) {}
+    l = n >> e;
+    giant.assign(l + 1, T());
+    baby.assign(n + 1, T());
+  }
+  PointAddRangeSum(const vector<T> &ts) {
+    n = ts.size();
+    for (e = 0; 1 << (2 * e) < n; ++e) {}
+    l = n >> e;
+    giant.assign(l + 1, T());
+    baby.assign(n + 1, T());
+    T sumsum = T();
+    for (int x = 0; x <= l; ++x) {
+      giant[x] = sumsum;
+      const int limY = min((x + 1) << e, n + 1);
+      T sum = T();
+      for (int y = x << e; y < limY; ++y) {
+        baby[y] = sum;
+        if (y < n) sum += ts[y];
+      }
+      sumsum += sum;
+    }
+  }
+  void add(int a, T val) {
+    assert(0 <= a); assert(a < n);
+    for (int x = (a >> e) + 1; x <= l; ++x) giant[x] += val;
+    const int limY = min(((a >> e) + 1) << e, n + 1);
+    for (int y = a + 1; y < limY; ++y) baby[y] += val;
+  }
+  inline T get(int a) const {
+    return giant[a >> e] + baby[a];
+  }
+  inline T get(int a, int b) const {
+    return get(b) - get(a);
+  }
+};
 
 // get i
 // add val to [a, b)
@@ -18,7 +63,7 @@ template <class T> struct RangeAddRangeCount {
     n = ts.size();
     m = max<int>(sqrt(n), 1);
     l = n / m;
-    offset.assign(l + 1, 0);
+    offset.assign(l + 1, T());
     sorted.resize(l * m);
     for (int x = 0; x < l; ++x) build(x);
   }
@@ -67,37 +112,85 @@ template <class T> struct RangeAddRangeCount {
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
+#include <random>
 #include <utility>
 
 using std::cerr;
 using std::endl;
+using std::mt19937_64;
 using std::swap;
 
-unsigned xrand() {
-  static unsigned x = 314159265, y = 358979323, z = 846264338, w = 327950288;
-  unsigned t = x ^ x << 11; x = y; y = z; z = w; return w = w ^ w >> 19 ^ t ^ t >> 8;
+void unittest_PointAddRangeSum() {
+  {
+    PointAddRangeSum<unsigned> f(0);
+    assert(f.get(0) == 0);
+    assert(f.get(0, 0) == 0);
+  }
+  constexpr int MAX_N = 1'000;
+  constexpr int NUM_QUERIES = 1'000;
+  mt19937_64 rng;
+  for (int n = 1; n <= MAX_N; ++n) {
+    vector<long long> brt(n);
+    PointAddRangeSum<long long> f(n);
+    for (int q = 0; q < NUM_QUERIES; ++q) {
+      if (rng() & 1) {
+        const int a = rng() % n;
+        const long long val = static_cast<int>(rng());
+        brt[a] += val;
+        f.add(a, val);
+      } else {
+        int a = rng() % (n + 1);
+        int b = rng() % (n + 1);
+        if (a > b) swap(a, b);
+        long long expected = 0;
+        for (int i = a; i < b; ++i) expected += brt[i];
+        assert(f.get(a, b) == expected);
+      }
+    }
+  }
+  for (int n = 1; n <= MAX_N; ++n) {
+    vector<long long> brt(n);
+    for (int i = 0; i < n; ++i) brt[i] = static_cast<int>(rng());
+    PointAddRangeSum<long long> f(brt);
+    for (int q = 0; q < NUM_QUERIES; ++q) {
+      if (rng() & 1) {
+        const int a = rng() % n;
+        const long long val = static_cast<int>(rng());
+        brt[a] += val;
+        f.add(a, val);
+      } else {
+        int a = rng() % (n + 1);
+        int b = rng() % (n + 1);
+        if (a > b) swap(a, b);
+        long long expected = 0;
+        for (int i = a; i < b; ++i) expected += brt[i];
+        assert(f.get(a, b) == expected);
+      }
+    }
+  }
 }
 
-void unittest() {
-  constexpr int NUM_QUERIES = 100'000;
-  for (int n = 1; n <= 100; ++n) {
+void unittest_RangeAddRangeCount() {
+  constexpr int MAX_N = 100;
+  constexpr int NUM_QUERIES = 10'000;
+  mt19937_64 rng;
+  for (int n = 0; n <= MAX_N; ++n) {
     vector<long long> brt(n);
-    for (int i = 0; i < n; ++i) brt[i] = static_cast<int>(xrand());
+    for (int i = 0; i < n; ++i) brt[i] = static_cast<int>(rng());
     RangeAddRangeCount<long long> f(brt);
     for (int i = 0; i < n; ++i) assert(f[i] == brt[i]);
     for (int q = 0; q < NUM_QUERIES; ++q) {
-      int a = xrand() % (n + 1);
-      int b = xrand() % (n + 1);
+      int a = rng() % (n + 1);
+      int b = rng() % (n + 1);
       if (a > b) swap(a, b);
-      const long long val = static_cast<int>(xrand());
-      if (xrand() & 1) {
-        f.add(a, b, val);
+      const long long val = static_cast<int>(rng());
+      if (rng() & 1) {
         for (int i = a; i < b; ++i) brt[i] += val;
+        f.add(a, b, val);
       } else {
-        const int actual = f.count(a, b, val);
         int expected = 0;
         for (int i = a; i < b; ++i) if (brt[i] < val) ++expected;
-        assert(actual == expected);
+        assert(f.count(a, b, val) == expected);
       }
       for (int i = 0; i < n; ++i) assert(f[i] == brt[i]);
     }
@@ -105,6 +198,7 @@ void unittest() {
 }
 
 int main() {
-  unittest(); cerr << "PASSED unittest" << endl;
+  unittest_PointAddRangeSum(); cerr << "PASSED unittest_PointAddRangeSum" << endl;
+  unittest_RangeAddRangeCount(); cerr << "PASSED unittest_RangeAddRangeCount" << endl;
   return 0;
 }
