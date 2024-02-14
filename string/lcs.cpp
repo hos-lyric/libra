3e41356a24749871d27ce5fa649a8964f9b3b541
hos <hos@hos.ac>
2024-02-14 16:37:04 +0900

    Add LCS by bitset

--- string/lcs.cpp
+++ string/lcs.cpp
@@ -1,8 +1,42 @@
+#include <algorithm>
 #include <utility>
 #include <vector>
 
 using std::pair;
 using std::vector;
+
+// LCS: longest common (not necessarily contiguous) subsequence
+// (i  ,j)->(i  ,j+1): 0...000...01
+// [as[i] = bs[j]]   : 0...01*...**
+// (i+1,j)->(i+1,j+1): 0...010...00
+template <class String> int lcs(const String &as, const String &bs) {
+  const int m = as.size(), n = bs.size();
+  String cs = as;
+  cs.insert(cs.end(), bs.begin(), bs.end());
+  std::sort(cs.begin(), cs.end());
+  cs.erase(std::unique(cs.begin(), cs.end()), cs.end());
+  const int csLen = cs.size();
+  const int len = (n + 63 - 1) / 63;
+  vector<unsigned long long> match(csLen * len, 0ULL);
+  for (int j = 0; j < n; ++j) {
+    const int b = std::lower_bound(cs.begin(), cs.end(), bs[j]) - cs.begin();
+    match[b * len + (j / 63)] |= 1ULL << (j % 63);
+  }
+  vector<unsigned long long> dp(len, 0ULL);
+  for (int i = 0; i < m; ++i) {
+    const int a = std::lower_bound(cs.begin(), cs.end(), as[i]) - cs.begin();
+    unsigned long long carry = 0;
+    for (int k = 0; k < len; ++k) {
+      const unsigned long long f = match[a * len + k];
+      const unsigned long long g = dp[k] - carry - (~dp[k] & f);
+      (dp[k] |= f) &= g;
+      carry = g >> 63;
+    }
+  }
+  int ans = 0;
+  for (int k = 0; k < len; ++k) ans += __builtin_popcountll(dp[k]);
+  return ans;
+}
 
 // LCS: longest common (not necessarily contiguous) subsequence
 // \Delta[k] LCS(as[0, k), bs[l, r)) = [l <  f[k][r]]
@@ -61,10 +95,12 @@ template <class String> PreSubLCS preSubLCS(const String &as, const String &bs) 
 #include <assert.h>
 #include <stdio.h>
 #include <iostream>
+#include <random>
 #include <string>
 
 using std::cerr;
 using std::endl;
+using std::mt19937_64;
 using std::string;
 
 template <class T> bool chmax(T &t, const T &f) { if (t < f) { t = f; return true; } return false; }
@@ -83,6 +119,7 @@ template <class String> void test(const String &as, const String &bs, int numQue
     if (r < n) chmax(dp[k][l][r + 1], dp[k][l][r]);
     if (k < m && r < n && as[k] == bs[r]) chmax(dp[k + 1][l][r + 1], dp[k][l][r] + 1);
   }
+  assert(dp[m][0][n] == lcs(as, bs));
   for (int k = 0; k < m; ++k) for (int r = 0; r <= n; ++r) {
     assert(0 <= psl.f[k][r]); assert(psl.f[k][r] <= r);
     for (int l = 0; l <= r; ++l) {
@@ -120,6 +157,8 @@ template <class String> void test(const String &as, const String &bs, int numQue
 }
 
 void unittest() {
+  test(string("test"), string("set"), -1);
+
   // Alves, Caceres, Song, An all-substrings common subsequence algorithm
   test(string("yxxyzyzx"), string("yxxyzxyzxyxzx"), -1);
 
@@ -141,10 +180,35 @@ void unittest() {
     }
     cerr << "[unittest] DONE sigma = 2, m = " << m << ", n = " << n << endl;
   }
+
+  {
+    mt19937_64 rng;
+    for (int caseId = 0; caseId < 100; ++caseId) {
+      const int m = rng() % 201;
+      const int n = rng() % 201;
+      vector<long long> as(m), bs(n);
+      for (int i = 0; i < m; ++i) as[i] = rng();
+      for (int j = 0; j < n; ++j) bs[j] = rng();
+      test(as, bs, 10);
+    }
+    cerr << "[unittest] DONE random cases" << endl;
+  }
+}
+
+// https://loj.ac/p/6564
+void loj_6564() {
+  int M, N;
+  for (; ~scanf("%d%d", &M, &N); ) {
+    vector<int> A(M), B(N);
+    for (int i = 0; i < M; ++i) scanf("%d", &A[i]);
+    for (int j = 0; j < N; ++j) scanf("%d", &B[j]);
+    const int ans = lcs(A, B);
+    printf("%d\n", ans);
+  }
 }
 
 // https://judge.yosupo.jp/problem/prefix_substring_lcs
-void yosupo__prefix_substring_lcs() {
+void yosupo_prefix_substring_lcs() {
   int Q;
   char S[1001], T[1001];
   for (; ~scanf("%d", &Q); ) {
@@ -164,7 +228,8 @@ void yosupo__prefix_substring_lcs() {
 }
 
 int main() {
-  unittest(); cerr << "PASSED unittest" << endl;
-  // yosupo__prefix_substring_lcs();
+  // unittest(); cerr << "PASSED unittest" << endl;
+  // yosupo_prefix_substring_lcs();
+  loj_6564();
   return 0;
 }
